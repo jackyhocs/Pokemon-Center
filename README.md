516224967a1c910dd98567ff54478ec95e159453
Jacky Ho <jackyho97@hotmail.com>
2017-11-09 02:17:54 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Pokemon Center
-
-Set this as your homepage to discover Pokemon! (to be deployed soon)
-Currently a work-in-progress
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './js/Root.jsx'
-
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root')
-);
