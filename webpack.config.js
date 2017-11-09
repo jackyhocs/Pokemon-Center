02d2543592a676a057e5c1108a99c2ef4ea7b683
Jacky Ho <jackyho97@hotmail.com>
2017-11-09 02:12:09 -0500

    Build?

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,5 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
-  /*
-  resolve: {
-    extensions: [ '*', '.js', '.jsx','.png' ]
-  },
-  */
   plugins: [HtmlWebpackPluginConfig]
 }
